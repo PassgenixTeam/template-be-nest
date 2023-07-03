4f5551d25c116d61b44f1a2991bba039b85d803a
nmthangdn2000 <uptimumdn2000@gmail.com>
2023-07-03 16:29:13 +0700

    fix AuthGuard jwt

--- libs/core/src/guards/jwt-auth/ws-jwt-auth.guard.ts
+++ libs/core/src/guards/jwt-auth/ws-jwt-auth.guard.ts
@@ -1,8 +1,7 @@
 import { ExecutionContext, Inject, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Reflector } from '@nestjs/core';
-import { ROLE } from '@app/common';
-import { ROLES_KEY } from '@app/common/constants/constant';
+import { IS_PUBLIC_KEY } from '@app/core/decorators';
 
 @Injectable()
 export class WsJwtAuthGuard extends AuthGuard('jwt') {
@@ -12,13 +11,16 @@ export class WsJwtAuthGuard extends AuthGuard('jwt') {
   }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const roles = this.reflector?.get<ROLE[]>(ROLES_KEY, context.getHandler());
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
 
     const req = context.switchToWs().getClient();
 
     const bearerToken = req.handshake.headers.authorization?.trim();
 
-    if (roles?.includes(ROLE.GUEST) && !bearerToken) {
+    if (isPublic && !bearerToken) {
       return true;
     }
 
